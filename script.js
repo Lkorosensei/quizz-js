161df2babda68fe30e8c6aca14dd8ebcacd9237d
isamir <samir.ibrahim38@outlook.fr>
2023-11-27 14:50:38 +0100

    avance afpa finir à la maison

--- script.js
+++ script.js
@@ -51,44 +51,100 @@ const questions = [
 
 /********* NE PAS MODIFIER AU DESSUS DE CETTE LIGNE *********/
 
-let affichageQuestions = document.getElementById("question");
-let affichageReponse = document.getElementById("answers");
 // let reponse = document.createElement("li");
 // reponse.classList.add("answer");
 // affichageReponse.appendChild(reponse);
-console.log("affichage réponse : ",affichageReponse);
-console.log("");
-console.log("");
+
 
 /*************************/
 /* Contenu du DOM chargé */
 /*************************/
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("objet nodeJs des questions :",questions);
-  console.log("affichage des questions : ",affichageQuestions.innerText = questions[0].question);
+  let affichageQuestions = document.getElementById("question");
+  let affichageReponse = document.getElementById("answers");
+  let score = document.getElementById("score").innerText;
+  console.log("score :",score);
+  console.log("affichage réponse : ",affichageReponse);
   console.log("");
-  questions.forEach(compteurQuestions => {
-    console.log("compteur questions :",compteurQuestions);
-    let reponseQuestions = compteurQuestions.answers;
-    console.log("reponseQuestions :",reponseQuestions);
-    affichageQuestions.innerText = compteurQuestions.question;
-    reponseQuestions.forEach(compteurReponse => {
-      console.log("compteurReponse :", compteurReponse);
-      console.log("");
-      let reponse = document.createElement("li");
-      reponse.classList.add("answer");
-      reponse.innerText = compteurReponse;
-      affichageReponse.appendChild(reponse)
-    });
-  });
+  console.log("");
 
-  
-  // for (let i = 0; i < questions.length; i--) {
-  //   const element = questions[i].question;
-  //   console.log(element);
-  //   affichageQuestions.innerText = element
-  // }
+  let indexQuestionNodeJS = 0;
 
+  afficherQuestions()
+    function afficherQuestions(params) {
+      affichageQuestions.innerText = questions[indexQuestionNodeJS].question;
+      
+      affichageReponse.innerHTML = "";  
+      afficherReponses()
+    }
+
+
+    function afficherReponses(params) {
+      questions[indexQuestionNodeJS].answers.forEach(compteurReponse => {
+        let reponse = document.createElement("li");
+        reponse.classList.add("answer");
+        reponse.innerText = compteurReponse;
+        affichageReponse.appendChild(reponse);
+      })
+    }
 // démarrage du quizz
-  
+  affichageReponse.addEventListener("click", (event) => {
+    
+    console.log("event :",event);
+    console.log("event target :", event.target);
+    console.log("cible touche texte :",event.target.innerText);
+    console.log("touche réponse clické ? :",event.target.classList.contains("answer"));
+    console.log("liste réponses : ", event.target.parentNode.children);
+    console.log("index/réponse N°",Array.from(event.target.parentNode.children).indexOf(event.target));
+
+    if (event.target.classList.contains("answer")) {
+      const indexReponseClicker = Array.from(event.target.parentNode.children).indexOf(event.target);
+
+      if (indexReponseClicker === questions[indexQuestionNodeJS].correctAnswerIndex) {
+        console.log("Bonne réponse !");
+        indexQuestionNodeJS++
+        console.log("score :",score);
+        console.log("indexQuestionsNodeJS :",indexQuestionNodeJS);
+
+        if (indexQuestionNodeJS < questions.length) {
+          afficherQuestions();
+        } else {
+          affichageQuestions.innerText = "Merci d'avoir répondu :) !"
+          affichageReponse.innerHTML = "";
+          console.log(affichageQuestions);
+        }
+      } else {
+        indexQuestionNodeJS++
+        afficherQuestions()
+        
+      }
+    }
+  })
 });
+
+
+// console.log("objet nodeJs des questions :",questions);
+// console.log("affichage de la 1ère questions : ",affichageQuestions.innerText = questions[0].question);
+// console.log("");
+// affichageQuestions.innerText = questions[0].question;
+// questions.forEach(compteurQuestions => {
+//   console.log("compteur questions :",compteurQuestions);
+//   let reponseQuestions = compteurQuestions.answers;
+//   console.log("reponseQuestions :",reponseQuestions);
+//   affichageQuestions.innerText = compteurQuestions.question;
+//   reponseQuestions.forEach(compteurReponse => {
+//     console.log("compteurReponse :", compteurReponse);
+//     console.log("");
+//     let reponse = document.createElement("li");
+//     reponse.classList.add("answer");
+//     reponse.innerText = compteurReponse;
+//     affichageReponse.appendChild(reponse)
+//   });
+// });
+
+
+// // for (let i = 0; i < questions.length; i--) {
+// //   const element = questions[i].question;
+// //   console.log(element);
+// //   affichageQuestions.innerText = element
+// // }
